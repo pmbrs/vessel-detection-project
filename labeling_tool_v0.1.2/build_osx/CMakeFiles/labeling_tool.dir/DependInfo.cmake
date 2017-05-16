553ea7bc216bec73d154b6e0e039b82d49481ae1
Francisco Maria Calisto <email@franciscocalisto.me>
2017-05-16 16:23:47 +0100

    [ADD] Video

---
+++ labeling_tool_v0.1.2/build_osx/CMakeFiles/labeling_tool.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/francisco/GitHub/vessel-detection-project/labeling_tool_v0.1.2/labeling_tool.cpp" "/Users/francisco/GitHub/vessel-detection-project/labeling_tool_v0.1.2/build_osx/CMakeFiles/labeling_tool.dir/labeling_tool.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ labeling_tool_v0.1.2/build_osx/CMakeFiles/labeling_tool.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/labeling_tool.dir/labeling_tool.cpp.o"
+  "labeling_tool.pdb"
+  "labeling_tool"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/labeling_tool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
