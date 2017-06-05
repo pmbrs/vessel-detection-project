09851e9f68f33680b16d12a3a2df85bcd083e947
FMCalisto <FMCalisto@users.noreply.github.com>
2017-06-05 18:28:43 +0100

    [FIX] Rectangle Function

--- src/main.m
+++ src/main.m
@@ -1,52 +1,39 @@
 %% STARTING
 
-function main
-    clear
-    clc
-    close all
+%function main
+ clear all, close all
+ 
+ 
+ 
     
-    imgBkg = imread('../Frames/frame0000.jpg');
-    
-    baseBkg = 0; % Initial Frame: 0 %
-    baseNum = 0;
+    baseBkg = 13; % Initial Frame: 0 %
+    baseNum = 13;
     
     nTotalFrames = 1536; % Total: 1536
     
     thr = 10;%30
-    minArea = 10;%10
-    maxArea = 40;%50
+    thr_global = 180;
+    thr_diff = 18;
+    
+    minArea = 100;%10
+    maxArea = 1000;%50
     alfa = 0.10;%0.10
     
     nFrameBkg = 1000;
-    step = 15;
-    Bkg = zeros(size(imgBkg));
+    step = 1;
+   
     
     mainFigure = figure(1);
     
     numKeyFrames = 0;
     
     se = strel('disk',3);
-
-    for i = 0 : step : nFrameBkg
-
-        imgfr = imread(sprintf('../Frames/frame%.4d.jpg', ...
-                        baseNum + i));
-
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-        Y = imgfr;
-        Bkg = alfa * double(Y) + (1 - alfa) * double(Bkg);
-
-        imgUInt8 = uint8(Bkg);
-    end
     
     % ------------------ END Backgroud ------------------ %
 
     % --------------------------------------------------- %
 
-    imgBkgBase = imgUInt8; % Imagem de background
+    %imgBkgBase = imgUInt8; % Imagem de background
 
     % -------------------- ROI -------------------------- %
     % Remove object intersection
@@ -55,39 +42,67 @@ function main
     stepRoi = 10;
     nFrameROI = nTotalFrames;  % 23354 Frames used to compute background image
 
-    for i = baseNum : stepRoi : nFrameROI
-
+    %for k = baseNum : stepRoi : nFrameROI
+    for k = 1 : 100  
         imgfrNew = imread(sprintf('../Frames/frame%.4d.jpg', ...
-                        baseNum + i));
+                        baseNum + k));
 
+                    
+        if k == 48
+            disp('')
+        end
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-        sprintf('ROI %d',i);
+        sprintf('ROI %d',k);
         hold off
         
         %imshow(imgfrNew); %% Caminho rectangulos amarelos - Background 
         hold on
         
-        imgdif = (abs(double(imgBkgBase(:,:,1))-double(imgfrNew(:,:,1)))>thr) | ...
-            (abs(double(imgBkgBase(:,:,2))-double(imgfrNew(:,:,2)))>thr) | ...
-            (abs(double(imgBkgBase(:,:,3))-double(imgfrNew(:,:,3)))>thr);
+        imgdif = (abs(double(imgfrNew(:,:,1)))>thr_global) | ...
+            (abs(double(imgfrNew(:,:,2))-double(imgfrNew(:,:,1)))>thr_diff) | ...
+            (abs(double(imgfrNew(:,:,3))-double(imgfrNew(:,:,1)))>thr_diff);
     
     
         bw = imclose(imgdif,se);
-        str = sprintf('Frame: %d',i); title(str);
+        str = sprintf('Frame: %d',k); title(str);
         
         % ----------------------------------------------------------- %
         imshow(bw);  %%Mete Background preto ao mesmo tempo
         % ----------------------------------------------------------- %
         
+        %imshow(bw)
+        [lb num]=bwlabel(bw);
+        regionProps = regionprops(lb,'area','FilledImage','Centroid');
+        
+        %inds = find(minArea < [regionProps.Area] < maxArea);
+        inds = [];
+        for k = 1 : length(regionProps)
+            if find([regionProps(k).Area] < maxArea & [regionProps(k).Area] > minArea)
+                inds = [ inds k ];
+            end
+        end
+
+        regnum = length(inds);
+
+        if regnum
+            for j=1:regnum
+                [lin, col]= find(lb == inds(j));
+                upLPoint = min([lin col]);
+                dWindow  = max([lin col]) - upLPoint + 1;
+
+                rectangle('Position',[fliplr(upLPoint) fliplr(dWindow)],'EdgeColor',[1 1 0],...
+                    'linewidth',2);
+            end
+        end
+        
         drawnow
-        clf(mainFigure, 'reset');
+        %clf(mainFigure, 'reset');
         
     end
     
-end
 
 % filename = fullfile(matlabroot,'examples','matlab','mydataVesselsC.txt');
 % fileID = fopen(filename);
