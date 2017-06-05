1b65eea26a50364ec82dd87e4c57307c609a524b
FMCalisto <FMCalisto@users.noreply.github.com>
2017-06-05 13:58:18 +0100

    [ADD] Background Basic Variables

--- src/main.m
+++ src/main.m
@@ -1,1 +1,14 @@
 %% STARTING
+
+function main
+    clear
+    clc
+    close all
+   
+    imgBk = imread('../Frames/frame0000.jpg');
+    
+    baseBkg = 0;
+    baseNum = 0;
+    
+    nTotalFrames = 4800;
+end
