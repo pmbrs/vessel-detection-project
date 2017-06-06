35572a1a05489031b48313ee11ed46202f2961a5
Joao Zeferino <jp.zef@hotmail.com>
2017-06-06 19:03:09 +0100

    [ADD] Starting Algorithm for Spacial Validation

--- src/main.m
+++ src/main.m
@@ -9,7 +9,7 @@ load vesselLabels.txt;
 
 % -------------------- CONST ---------------------- %
 RegionBuffer = [];
-stepRoi = 25;
+stepRoi = 5;
 
 %baseBkg = 13; % Initial Frame: 0 %
 baseNum = 13;
@@ -19,14 +19,17 @@ baseNum = 13;
 nTotalFrames = 1533; % Total: 1533
 nInitialFrame = 12;  % Initial Boat: 12
 
-thr_global = 180; % 180
-thr_diff = 60;    % 18
+thr_global = 150; % 180
+thr_diff = 18;    % 18 %60 fails detecting the boat sometimes
 
 minArea = 100;  % 100
 maxArea = 1000; % 1000
 %alfa = 0.10;    % 0.10
 
 %nFrameBkg = 1000;   
+
+distanceBetweenVessels = 100;
+array_inds = [];
 
 mainFigure = figure(1);
 
@@ -81,8 +84,53 @@ for k = nInitialFrame : stepRoi : nTotalFrames
     end
 
     regnum = length(inds);
-
+    
     if regnum
+        % ----------------------------------------------------------- %
+        %%%Spacial Validation
+        % ----------------------------------------------------------- %
+%         for k=1:regnum
+%             for m=1:regnum
+%                %run all inds to search for inds that are too close
+%                vesselAX = regionProps(inds(k)).Centroid(1,1);
+%                vesselAY = regionProps(inds(k)).Centroid(1,2);
+%                vesselBX = regionProps(inds(m)).Centroid(1,1);
+%                vesselBY = regionProps(inds(m)).Centroid(1,2);
+%                
+%                distBetweenVessels = [vesselAX, vesselAY; ...
+%                    vesselBX, vesselBY];
+%                pdistBetweenVessels = pdist(distOfNeighborM, 'euclidean');
+%                
+%                if pdistBetweenVessels < distanceBetweenVessels
+%                   %remove inds k and m from inds
+%                   
+%                   %ind = [1 4 7] ; % indices to be removed
+%                   %A(ind) = []; % remove
+%                   
+%                   for n=1:size(inds)
+%                       if inds(n) == k_value;
+%                           
+%                           
+%                           array_inds = [array_inds inds(n)];
+%                       end
+%                   end
+%                   
+%                end
+%                
+%                
+%             end
+%         end
+        
+        %%USE if spacial validation is not used
+        %array_inds = inds;
+        
+        % ----------------------------------------------------------- %
+        %%%Temporal Buffer
+        % ----------------------------------------------------------- %
+        %%change buffer lines from 1-14 to 2-15
+        %%frame 15 will be overwriten
+        
+        %doing boxes on approved inds
         for j=1:regnum
             [lin, col]= find(lb == inds(j));
             upLPoint = min([lin col]);
@@ -90,26 +138,15 @@ for k = nInitialFrame : stepRoi : nTotalFrames
 
             rectangle('Position',[fliplr(upLPoint) fliplr(dWindow)],'EdgeColor',[1 1 0],...
                 'linewidth',2);
+            
+            %%%Temporal Buffer
+            %%add regionProps to j(index) of the buffer
+            
+            
+            
         end
     end
 
     drawnow
 
 end
-
-
-
-% function [mask_v, targets] = Vessel_detection(l, SR, R_threshold, dif_threshold)
-% 
-% % l         input image
-% % mask_v    binary mask
-% % features  array of regions features
-% 
-% % vessel detection
-% % mask = (max(I, [], 3) > R_threshold) = ([max](I, [], 3) - min(1, [], 3));
-% 
-% SE = strel('disk', SR);
-% mask_v = imdilate(imgdif, SE);
-% targets = extract_targets(mask_v); % extract blob features
-%     
-% end
